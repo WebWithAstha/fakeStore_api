b18302c0ce490799de55a6b082bd483c3c894971
WebWithAstha <lodhiastha85@gmail.com>
2024-06-20 15:33:53 +0530

    card modified

--- src/components/ProdCard.jsx
+++ src/components/ProdCard.jsx
@@ -1,17 +1,23 @@
 import React, { useContext } from 'react'
 import { productcontext } from '../contexts/ProductContext'
+import { Link } from 'react-router-dom'
 
 const ProdCard = ({ index }) => {
   const [prods, setprods] = useContext(productcontext)
   const prod = prods[index]
 
   return (
-    <div className="card w-full bg-slate-100 border border-slate-300  p-4">
-      <div className="img w-full h-48 bg-white p-4">
-        <img className='w-full h-full object-contain' src={prod.image} alt="" />
+    <Link to={`/product/${prod.id}`}>
+    <div className="card w-full cursor-pointer group bg-slate-100 border border-slate-300  p-4">
+      <div className="img w-full h-48 bg-white p-6">
+        <img className='w-full h-full object-contain group-hover:scale-110 transition-transform duration-500' src={prod.image} alt="" />
       </div>
-      <h2 className='mt-2'>{prod.title.slice(0, 20) + ".."}</h2>
+      <div className="flex mt-2 items-center justify-between">
+        <h2 className=''>{prod.title.slice(0, 15) + ".."}</h2>
+        <i className="ri-arrow-right-s-line group-hover:drop-shadow-[2px_2px_2px]"></i>
+      </div>
     </div>
+          </Link>
   )
 }
 
